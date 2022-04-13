ee6e21b503993c5f430430aa83a1884ce0c04c89
Paul Grogan <paul@ptgrogan.com>
2022-04-12 23:21:48 -0400

    add mass and volume to every demand

--- src/main/java/edu/mit/spacenet/io/gson/Demand.java
+++ src/main/java/edu/mit/spacenet/io/gson/Demand.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class Demand {
 	public Resource resource;
 	public double amount;
+	public double mass;
+	public double volume;
 	
 	public static Demand createFrom(edu.mit.spacenet.domain.resource.Demand demand) {
 		Demand d = new Demand();
 		d.resource = Resource.createFrom(demand.getResource());
 		d.amount = demand.getAmount();
+		d.mass = demand.getMass();
+		d.volume = demand.getVolume();
 		return d;
 	}
 	

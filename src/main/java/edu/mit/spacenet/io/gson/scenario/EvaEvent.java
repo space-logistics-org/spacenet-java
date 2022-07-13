5cfc814a87928edb3ed2fedca0b61f0d84a8cec2
Paul Grogan <paul@ptgrogan.com>
2022-07-13 16:24:13 -0400

    fix units for eva duration (#52)

--- src/main/java/edu/mit/spacenet/io/gson/scenario/EvaEvent.java
+++ src/main/java/edu/mit/spacenet/io/gson/scenario/EvaEvent.java
@@ -32,9 +32,9 @@ public class EvaEvent extends Event {
     e.priority = event.getPriority();
     e.location = context.getJsonIdFromJavaObject(event.getLocation());
     e.vehicle = context.getJsonIdFromJavaObject(event.getVehicle());
-    e.evaDuration =
-        PeriodDuration.of(Period.ofDays((int) event.getEvaDuration()), Duration.ofSeconds(
-            (long) ((event.getEvaDuration() - (int) event.getEvaDuration()) * 24 * 60 * 60)));
+    e.evaDuration = PeriodDuration.of(Period.ofDays((int) (event.getEvaDuration() / 24d)),
+        Duration.ofSeconds((long) ((event.getEvaDuration() / 24d - (int) (event.getEvaDuration() / 24d))
+            * 24 * 60 * 60)));
     e.elementStates = new HashMap<UUID, Integer>();
     for (I_Element element : event.getStateMap().keySet()) {
       if (event.getStateMap().get(element) == null) {
@@ -70,8 +70,8 @@ public class EvaEvent extends Event {
       }
     }
     e.setStateMap(stateMap);
-    e.setEvaDuration(evaDuration.getPeriod().getDays()
-        + evaDuration.getDuration().getSeconds() / (24 * 60 * 60d));
+    e.setEvaDuration(evaDuration.getPeriod().getDays() * 24
+        + evaDuration.getDuration().getSeconds() / (60 * 60d));
     return e;
   }
 
